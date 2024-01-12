5607bd9384ecd9bab900acd43ce1b6338dc3b181
Francisco_Esteban <rept2000@gmail.com>
2024-01-12 21:04:05 +0100

    pruebasunitarias

---
+++ __test__/test.suma.js
@@ -0,0 +1,10 @@
+const sum = require('../utils/suma');
+
+test('sumar 1 mas 2', () =>
+
+expect(sum(1,2)).toBe(3);
+
+const sum = require('../utils/suma'); () => {
+
+})
+
---
+++ utils/suma.js
@@ -0,0 +1,7 @@
+//FUNCIÓN A PROBAR
+
+function sum(a, b) {
+    return a + b;
+}
+module.exports = sum;
+
